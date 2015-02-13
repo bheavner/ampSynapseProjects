3386e105f4d7a0cf329332d0f76ae42fc0d91a10
Ben Heavner <bheavner@systemsbiology.org>
2015-02-12 16:11:15 -0800

    adding reformatting code to transpose merged recount file and remove samples to avoid for QC reasons

--- rnaseqAnalysis/normalize_readcounts.R
+++ rnaseqAnalysis/normalize_readcounts.R
@@ -1,4 +1,6 @@
-# This script normalizes the merged count_files produced by running merge_file_counts.R on SNAPR output. It does so by dowloading the merged read count files, processing them, and uploading new synapse objects with appropriate provenance.
+# This script normalizes the merged count_files produced by running merge_file_counts.R
+# on SNAPR output. It does so by dowloading the merged read count files, processing them,
+# and uploading new synapse objects with appropriate provenance.
 
 library(synapseClient)
 library(R.utils)
@@ -12,69 +14,69 @@ codeFile <- ("https://github.com/bheavner/ampSynapseProjects/blob/master/rnaseqA
 # The files for the first batch are:
 
 # ad_pilot_rnaseq_gene_id_counts.txt.gz ('syn3160436')
-# ad_pilot_rnaseq_gene_name_counts.txt.gz ('syn3160433')
 # ad_pilot_rnaseq_transcript_id_counts.txt.gz ('syn3160437')
 
 # psp_pilot_rnaseq_gene_id_counts.txt.gz ('syn3160443')
-# psp_pilot_rnaseq_gene_name_counts.txt.gz ('syn3160442')
 # psp_pilot_rnaseq_transcript_id_counts.txt.gz ('syn3160444')
 
 # mouse_tau_rnaseq_gene_id_counts.txt.gz ('syn3160706')
-# mouse_tau_rnaseq_gene_name_counts.txt.gz ('syn3160705')
 # mouse_tau_rnaseq_transcript_id_counts.txt.gz ('syn3160709')
 
-countFileSynapseIDs <- c('syn3160436', 'syn3160433', 'syn3160437', 'syn3160443', 'syn3160442', 'syn3160444', 'syn3160706', 'syn3160705','syn3160709' )
+#countFileSynapseIDs <- c('syn3160436', 'syn3160437', 'syn3160443', 'syn3160444', 'syn3160706', 'syn3160709' )
 
 for (mergedCountFile in countFileSynapseIDs) {
+    message("Normalizing ", mergedCountFile)
 
-  message("Normalizing ", mergedCountFile)
+    # Download file from Synapse
+    originalCountFile <- synGet(mergedCountFile)
 
-  # Download file from Synapse
-  originalCountFile <- synGet(mergedCountFile)
+    # unzip file and load for processing
+    localFilePath <- getFileLocation(originalCountFile)
 
-  # unzip file and load for processing
-  localFilePath <- getFileLocation(originalCountFile)
+    if(!file.exists(substr(localFilePath, 1, nchar(localFilePath) - 3))) {
+        gunzip(localFilePath)
+    }
 
-  if(!file.exists(substr(localFilePath, 1, nchar(localFilePath)-3))) {
-    gunzip(localFilePath)
-  }
+    localFilePath <- sub('.gz', '', localFilePath) #trim the .gz suffix
 
-  localFilePath <- substr(localFilePath, 1, nchar(localFilePath)-3) #trim the .gz suffix
+    rawCounts <- read.table(localFilePath, header = TRUE)
 
-  rawCounts <- read.table(localFilePath, header = TRUE)
+    # begin processing - first transpose b/c James did it differently than DGEList expects
+    transposedCounts <- t(rawCounts)
 
-  # begin processing - first transpose b/c James did it differently than DGEList expects
-  transposedCounts <- t(rawCounts)
+    # make DGEList object
+    expr <- DGEList(transposedCounts, group = rep(1, ncol(transposedCounts)))
 
-  # make DGEList object
-  expr <- DGEList(transposedCounts, group = rep(1,ncol(transposedCounts)))
+    # calculate normalization factors
+    normFactors <- calcNormFactors(expr, method = ("TMM"))
 
-  # calculate normalization factors
-  normFactors <- calcNormFactors(expr, method=("TMM"))
+    # use normaliztion factors to calculate cpm -
+    # per https://www.biostars.org/p/84087/, that's calculated as
+    # count / (library size * normalization factor))
 
-  # use normaliztion factors to calculate cpm -
-  # per https://www.biostars.org/p/84087/, that's calculated as
-  # count / (library size * normalization factor))
+    normalizedCpm <- cpm(normFactors)
 
-  normalizedCpm <- cpm(normFactors)
+    # write the data to local dir
 
-  # make it a data frame and write the table to local dir
-  normalizedCounts <- as.data.frame(t(normalizedCpm))
+    newFileName <- sub('.txt.gz', '', originalCountFile$properties$name)
+    newFileName <- paste0(newFileName, "_normalized.txt", sep="")
 
-  newFileName <- substr(originalCountFile$properties$name, 1, nchar(originalCountFile$properties$name)-7) # strip .txt.gz
-  newFileName <- paste(newFileName, "_normalized.txt", sep="")
+    write.table(normalizedCpm, newFileName, quote = FALSE, sep = "\t", row.names = TRUE)
 
-  write.table(normalizedCounts, newFileName, quote = FALSE, row.names = FALSE)
+    # package it up, then create a Synapse object for the output file and upload with provenance
 
-  # package it up, then create a Synapse object for the output file and upload with provenance
+    gzip(newFileName)
 
-  gzip(newFileName)
+    newFileName <- paste0(newFileName, ".gz", sep="")
 
-  newFileName <- paste(newFileName, ".gz", sep="")
+    parentId <- originalCountFile$properties$parentId
 
-  parentId <- originalCountFile$properties$parentId
+    normalizedCountFile <- File(newFileName, parentId = parentId)
 
-  normalizedCountFile <- File(newFileName, parentId = parentId)
-
-  normalizedCountFile <- synStore(normalizedCountFile, activityName="CPM (using TMM) from edgeR normalization", used=list(list(name = "normalize_readcounts.R", url = codeFile, wasExecuted = T), list(entity=originalCountFile, wasExecuted=F)))
+    normalizedCountFile <- synStore(normalizedCountFile,
+                                    activityName="CPM (using TMM) from edgeR normalization",
+                                    used=list(list(name = "normalize_readcounts.R",
+                                                   url = codeFile, wasExecuted = T),
+                                              list(entity=originalCountFile,
+                                                   wasExecuted=F)))
 }
---
+++ rnaseqAnalysis/reformat_merged_readcounts.R
@@ -0,0 +1,90 @@
+# This script downloads the merged count_files produced by running merge_file_counts.R on SNAPR output, transposes the
+# dataframe so genes are rows and samples are columns, removes samples to be omitted for QC reasons, and reuploads
+# them to synapse with appropriate provenance that this was run.
+
+library(synapseClient)
+library(R.utils)
+library(edgeR)
+
+#Login to Synapse using credentials saved in .synapseConfig file
+synapseLogin()
+
+codeFile <- ("https://github.com/bheavner/ampSynapseProjects/blob/master/rnaseqAnalysis/reformat_merged_readcounts.R")
+
+# The files for the first batch are:
+
+# ad_pilot_rnaseq_gene_id_counts.txt.gz ('syn3160436')
+# ad_pilot_rnaseq_transcript_id_counts.txt.gz ('syn3160437')
+
+# psp_pilot_rnaseq_gene_id_counts.txt.gz ('syn3160443')
+# psp_pilot_rnaseq_transcript_id_counts.txt.gz ('syn3160444')
+
+# mouse_tau_rnaseq_gene_id_counts.txt.gz ('syn3160706')
+# mouse_tau_rnaseq_transcript_id_counts.txt.gz ('syn3160709')
+
+adCountFileSynapseIDs <- c('syn3160436', 'syn3160437')
+pspCountFileSynapseIDs <- c('syn3160443', 'syn3160444')
+mouseCountFileSynapseIDs <- c('syn3160706', 'syn3160709')
+
+# samples to exclude for QC reasons:
+
+adOmit  <- c('5215', 'NA98-300', 'NA02-092')
+pspOmit <- c('NA00-002', 'NA00-164', 'NA03-008', 'NA99-042', 'NA04-056', 'NA01-150', 'NA01-058', 'NA00-213', 'NA03-154', 'NA03-291', 'NA04-020')
+
+for (dataset in list(adCountFileSynapseIDs, pspCountFileSynapseIDs, mouseCountFileSynapseIDs)) {
+    if (identical(dataset, adCountFileSynapseIDs)) {
+        omit <- adOmit
+    } else if (identical(dataset, pspCountFileSynapseIDs)) {
+        omit <- pspOmit
+    } else {
+        omit <- NULL
+    }
+
+    for (mergedCountFile in dataset) {
+        message("Processing SynID: ", mergedCountFile)
+
+        # Download file from Synapse
+        originalCountFile <- synGet(mergedCountFile)
+
+        # unzip file and load for processing
+        localFilePath <- getFileLocation(originalCountFile)
+
+        if(!file.exists(sub('.gz', '', localFilePath))) {
+            gunzip(localFilePath)
+        }
+
+        localFilePath <- sub('.gz', '', localFilePath) #trim the .gz suffix
+
+        rawCounts <- read.table(localFilePath, header = TRUE)
+
+        # begin processing - first transpose b/c James did it differently than DGEList expects
+        transposedCounts <- t(rawCounts)
+
+        # remove samples that don't pass QC
+        keepSamples <- setdiff(colnames(transposedCounts), omit) #probably not the best way..
+        transposedCounts <- transposedCounts[, keepSamples]
+
+        # write the data to local dir
+
+        newFileName <- sub('.gz', '', originalCountFile$properties$name)
+
+        write.table(transposedCounts, newFileName, quote = FALSE, sep = "\t", row.names = TRUE)
+
+        # package it up, then create a Synapse object for the output file and upload with provenance
+
+        gzip(newFileName)
+
+        newFileName <- paste0(newFileName, ".gz", sep="")
+
+        parentId <- originalCountFile$properties$parentId
+
+        transposedCountFile <- File(newFileName, parentId = parentId)
+
+        transposedCountFile <- synStore(transposedCountFile,
+                                        activityName="Transposed merged readcount file",
+                                        used=list(list(name = "reformat_merged_readcounts.R",
+                                                       url = codeFile, wasExecuted = T),
+                                                  list(entity=originalCountFile,
+                                                       wasExecuted=F)))
+    }
+}
